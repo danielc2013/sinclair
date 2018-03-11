97c4e20b3f80be3c0a5c22a5cb2ae0a52865b436
Daniel Casias <dcasias@colotek.com>
2018-03-11 10:49:56 -0600

    add specs

--- spec/basicSpec.js
+++ spec/basicSpec.js
@@ -4,4 +4,19 @@ describe('Sinclair exists', () => {
   it('Should exist', () => {
     expect(sinclair).toBeDefined()
   })
+
+  it('Should allow custom template filepaths', () => {
+    let sampleSvc = require('../sinclair')(
+      'SampleSvc',
+      './spec/helpers/sample-sinclair.yml'
+    )
+    expect(sampleSvc).toBeDefined()
+  })
+})
+
+describe('Sinclair wraps services in requests', () => {
+  let sampleSvc = require('../sinclair')(
+    'SampleSvc',
+    './spec/helpers/sample-sinclair.yml'
+  )
 })
